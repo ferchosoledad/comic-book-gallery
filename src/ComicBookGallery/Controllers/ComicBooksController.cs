9a9a2baf959fd161d9e71f74b815dc0b81e9b722
Fernando Soledad <ferchosoledad@gmail.com>
2016-10-11 21:04:41 -0500

    Added Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,14 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-
-            }
-
-            return Content("Hello from the comic books gallery controller");
-            
+            return View();
         }
     }
 }
